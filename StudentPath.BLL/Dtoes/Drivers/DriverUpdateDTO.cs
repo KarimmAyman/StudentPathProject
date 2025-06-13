32a1079cbc7a059fbd9767e7ae8f14407ec0cd64
Mohamed Saber <mohameddebany255@gmail.com>
2025-06-13 17:36:03 +0300

    AddPhotoToEditDriver

--- StudentPath.BLL/Dtoes/Drivers/DriverUpdateDTO.cs
+++ StudentPath.BLL/Dtoes/Drivers/DriverUpdateDTO.cs
@@ -9,6 +9,7 @@ namespace StudentPath.BLL.Dtoes
     public class DriverUpdateDTO
     {
         // Basic Info
+        public IFormFile? PersonalPhoto { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
